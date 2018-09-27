d8f55b6a4abd26e0d1c62592521fb97a64554b51
Jessica Khau <j_khau@hotmail.com>
2018-09-27 15:21:45 -0400

    Finish fonctions

--- 07-fonctions.sh
+++ 07-fonctions.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+maFonction() {
+	echo "parametre : $1"
+}
+
+maFonction allo
+
+afficherInfo() {
+	echo "-----------------"
+	echo "Kernel : " `uname -rs`
+	echo "-----------------"
+}
