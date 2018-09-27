5081074d74e7ddc6ff3fb0ae4074ee2af3b9e15a
Jessica Khau <j_khau@hotmail.com>
2018-09-27 14:18:21 -0400

    Finished variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+#Assignation (sans $)
+prenom=David
+
+#Lecture de la variable, toujours avec $
+echo $prenom
+
+declare -i age=33 # variable est un entier
+declare -r age2=34 #variable en lecture seule, impossible à réaffecter
+
+cours="Cours de Linux"
+
+#Les variables systèmes: 
+echo Prog $0 avec premier param $1, deuxieme param $2, total est $#
+
