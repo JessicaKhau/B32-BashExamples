7b5ee8104ac0466740fa21e7037e8b3ff416acae
Jessica Khau <j_khau@hotmail.com>
2018-09-27 14:23:22 -0400

    Finished calculs

--- 03-calculs.sh
+++ 03-calculs.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+#bonne facon de faire des calculs
+echo $((10+10))
+
+# mauvaise facon
+echo 10+10
+
+read -p "nombre 1 : " nb1
+read -p "Nombre 2 : " nb2
+
+let resultat=$nb1*$nb2
+echo $resultat
